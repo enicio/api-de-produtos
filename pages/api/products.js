e6e317c3b9f38d170c6f9b43bff6f0f4f49ecc88
enicio <jessandro42@gmail.com>
2021-04-27 21:00:53 -0300

    com o arquivo json de produtos

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import products from '../../service/products.json'
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(products)
 }
