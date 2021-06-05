8e5cb7480fc80b8d943a212fb3f2496972e471d6
enicio <jessandro42@gmail.com>
2021-06-05 20:12:24 -0300

    incluida rota para pet products

---
+++ pages/api/petproducts.js
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import petproducts from '../../service/petproducts.json'
+
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
+
+  // Rest of the API logic
+  res.status(200).json(petproducts)
+}
